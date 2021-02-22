05c2208f6f91d44ccc642620055e13cab95dc704
allxll <mildwindyu@hotmail.com>
2021-02-22 13:49:19 +0800

    model blank

--- blog/models.py
+++ blog/models.py
@@ -10,7 +10,7 @@ class Post(models.Model):
     author = models.CharField(max_length=60, blank=True)
     main_text = models.TextField(blank=True)
     is_hidden = models.BooleanField(default=False)
-    reference = models.ManyToManyField('Paper', related_name='contained_post')
+    reference = models.ManyToManyField('Paper', related_name='contained_post', blank=True)
 #   attached_file = models.FileField()
     def __str__(self):
         return self.title[:20]
@@ -18,7 +18,7 @@ class Post(models.Model):
 
 def content_file_name(instance, filename):
     ext = filename.split('.')[-1] 
-    filename = f'{instance.id}.{ext}'
+    filename = f'{instance.id}.{ext}'   
     return os.path.join('papers', filename)
 
 class Paper(models.Model):
@@ -26,6 +26,6 @@ class Paper(models.Model):
     author = models.CharField(max_length=100, blank=True)
     journal = models.CharField(max_length=100, blank=True)
     year = models.CharField(max_length=4, blank=True, default='')
-    attachment = models.FileField(upload_to=content_file_name, null=True)
+    attachment = models.FileField(upload_to=content_file_name, blank=True)
     def __str__(self):
         return self.title
